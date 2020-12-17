0291dddca44b4454df9b2072dda2b186b758d190
WyattCodzOfficial <75955994+WyattCodzOfficial@users.noreply.github.com>
2020-12-17 09:43:57 -0500

    Create botConfig.js

---
+++ botConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: "TOKEN_HERE",
+    prefix: "PREFIX_HERE",
+    admins: [
+    "",   
+//admin id's here seperated by , but id's are in ""
+],
+    debug: true,
+    countChannel: "countChannelID"//the designated channel for counting's id
+};
